c0423dde0dc2cef9fda6ddd839b7f8140d3d2625
Jason Hills <jasonahills@gmail.com>
2020-05-20 20:06:07 -0400

    updates prod to 0.1.1

---
+++ docs/precache-manifest.75d2f868d8a1efc24f4ec933f0db7538.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d9eab0f3d26e9b667d696f65aa29ed4d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9759863fe09a194cb280",
+    "url": "/static/css/main.9c99a95f.chunk.css"
+  },
+  {
+    "revision": "9f5f8a9035fc93d882d7",
+    "url": "/static/js/2.04f8a5f9.chunk.js"
+  },
+  {
+    "revision": "90565ba0b43e1b95d2876295dfd7014d",
+    "url": "/static/js/2.04f8a5f9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9759863fe09a194cb280",
+    "url": "/static/js/main.0b8644aa.chunk.js"
+  },
+  {
+    "revision": "ae9e11758fe41ee6e87b",
+    "url": "/static/js/runtime-main.2d39ae87.js"
+  }
+]);
